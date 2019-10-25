a2969071bbf75517c6598de5ee62fd00ce6bc0b0
Jonas Holbech <jofh@kea.dk>
2019-10-25 11:59:55 +0200

    pwa part 2

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -4,7 +4,6 @@ const CACHE = "pwabuilder-page";
 
 // TODO: replace the following with the correct offline fallback page i.e.: 
 const offlineFallbackPage = "offline.html";
-//const offlineFallbackPage = "ToDo-replace-this-name.html";
 
 // Install stage sets up the offline page in the cache and opens a new cache
 self.addEventListener("install", function (event) {
